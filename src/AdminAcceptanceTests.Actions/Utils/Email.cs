477b2583c7b398c03060b22140a076e8d687969d
Tony Lawson <tony.lawson@bjss.com>
2020-04-08 10:27:51 +0100

    lifted email driver from dev integration tests

---
+++ src/AdminAcceptanceTests.Actions/Utils/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdminAcceptanceTests.Actions.Utils
+{
+    public sealed class Email
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string PlainTextBody { get; set; }
+
+        public string HtmlBody { get; set; }
+    }
+}
---
+++ src/AdminAcceptanceTests.Actions/Utils/EmailServerDriver.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AdminAcceptanceTests.Actions.Utils
+{
+    public sealed class EmailServerDriver
+    {
+        public static async Task<int> GetEmailCountAsync(string hostUrl)
+        {
+            var emailList = await FindAllEmailsAsync(hostUrl);
+            return emailList.Count();
+        }
+
+        public static async Task<IEnumerable<Email>> FindAllEmailsAsync(string hostUrl)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            using var client = new HttpClient();
+            response = await client.GetAsync($"{hostUrl}:1080/email");
+
+            var responseContent = JToken.Parse(await response.Content.ReadAsStringAsync());
+
+            return responseContent.Select(x => new Email
+            {
+                PlainTextBody = x.SelectToken("text").ToString().Trim(),
+                HtmlBody = x.SelectToken("html").ToString().Trim(),
+                Subject = x.SelectToken("subject").ToString(),
+                From = x.SelectToken("from").First().SelectToken("address").ToString(),
+                To = x.SelectToken("to").First().SelectToken("address").ToString(),
+            });
+        }
+
+        public static async Task ClearAllEmailsAsync(string hostUrl)
+        {
+            using var client = new HttpClient();
+            await client.DeleteAsync(new Uri($"{hostUrl}:1080/email/all"));
+        }
+    }
+}
