5f69447752f976bbf6b45a9ab044dce78bcb6b47
Tony Lawson <tony.lawson@bjss.com>
2020-05-06 11:18:41 +0100

    changed locator on login button to something unique

--- src/AdminAcceptanceTests.Objects/Pages/Login.cs
+++ src/AdminAcceptanceTests.Objects/Pages/Login.cs
@@ -7,7 +7,7 @@ namespace AdminAcceptanceTests.Objects.Pages
     {
         public By Username => By.Id("EmailAddress");
         public By Password => By.Id("Password");
-        public By LoginButton => By.CssSelector("button[type=submit]");
+        public By LoginButton => CustomBy.DataTestId("login-submit-button");
         public By RequestAnAccountLink => CustomBy.DataTestId("request-account-link");
         public By ErrorSummaryBox => CustomBy.DataTestId("error-summary");
         public By ForgotPassword => CustomBy.DataTestId("forgot-password-link");
